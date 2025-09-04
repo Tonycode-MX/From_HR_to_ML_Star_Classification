7975710aa318003347ac0f4509f256fdd950a977
TonyCol <luanrama5@gmail.com>
2025-09-03 19:29:11 -0500

    Se creó el primer stage para la importación de datos desde Gaia Archive o desde un archivo local CSV. Se añadieron prints para verificar las columnas importadas y el número de filas. También se añadió un mensaje de éxito al finalizar la importación.

--- modules/main.py
+++ modules/main.py
@@ -1,23 +1,56 @@
-from astroquery.gaia import Gaia
-import numpy as np
+# Main module for executing stages
+
+# LIBRARIES
+
+# Standard
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy
+import os, sys
 
-Gaia.login()  # puedes hacerlo como guest si no te registras
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
 
-job = Gaia.launch_job_async("""
-SELECT TOP 30000
-       ra, dec, parallax, parallax_error,
-       phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag,
-       ruwe, phot_bp_rp_excess_factor
-FROM gaiadr3.gaia_source
-WHERE parallax_over_error > 10
-  AND ruwe < 1.4
-  AND parallax > 2
-""")
+# External modules
+from modules.module_data_path import data_path, plot_data_path
+from data.data_import import gaia_data_import
 
-results = job.get_results()
-df = results.to_pandas()
+# Number of stages to execute (1 to )
+stages = [1]
 
-df["BP_RP"] = df["phot_bp_mean_mag"] - df["phot_rp_mean_mag"]
-d_pc = 1000.0 / df["parallax"]   # distancia en parsecs (parallax en mas)
-df["M_G"] = df["phot_g_mean_mag"] + 5 - 5*np.log10(d_pc)
+# STAGE 1: Data Import
+def stage1():
+
+    """
+    Main script to choose how to import the dataset.
+    Uncomment the preferred option:
+      1) Import directly from Gaia archive (astroquery)
+      2) Import from local CSV file
+    """
+
+    # OPTION 1: Import from Gaia
+    df = gaia_data_import()  # guest login by default
+
+    # # (Optional) Save the imported DataFrame as CSV in /data
+    # save_path = os.path.join(data_path, "dataset_from_gaia.csv")
+    # df.to_csv(save_path, index=False)
+    # print(f"Dataset saved to {save_path}")
+
+    # OPTION 2: Import from local CSV in data folder
+    #data_path = data_path()
+    #dataset = os.path.join(data_path, "dataset.csv")
+    #df = pd.read_csv(dataset)
+
+    # Preview
+    print("\nImported dataset preview:")
+    print(df.head())
+
+
+# Execute stages
+if __name__ == '__main__': 
+    
+    if 1 in stages:
+        stage1()
