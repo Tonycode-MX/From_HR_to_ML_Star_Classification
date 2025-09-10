df2a7b6881e94c52c103c6accbb8bd1fabd34a48
TonyCol <luanrama5@gmail.com>
2025-09-10 15:29:29 -0500

    Se realizaron cambios en el stage 1 para limpiar el dataset importado y guardar el dataset limpio en un archivo CSV. Se realizaron cambios en el stage 2 para contar las estrellas de cada tipo y guardar el dataset clasificado en un archivo CSV. Se agregaron mensajes informativos adicionales para mejorar la comprensión del proceso. Se corrigieron errores tipográficos en los nombres de los archivos guardados.

--- modules/main.py
+++ modules/main.py
@@ -16,14 +16,15 @@ if ROOT not in sys.path:
 
 # External modules
 from modules.module_data_path import data_path, plot_data_path
+from modules.module_data_cleaning import nans_elimination
 from data.data_import import gaia_data_import
+from modules.module_utils import add_color_magnitude_indices, label_star, star_counts, save_dataframe
 
 # Number of stages to execute (1 to )
 stages = [1]
 
-# STAGE 1: Data Import
+# STAGE 1: Data Import and cleaning
 def stage1():
-
     """
     Main script to choose how to import the dataset.
     Uncomment the preferred option:
@@ -31,21 +32,51 @@ def stage1():
       2) Import from local CSV file
     """
 
+    # Get data path
+    data_folder = data_path()
+
     # OPTION 1: Import from Gaia
     df = gaia_data_import()  # guest login by default
-
-    # # (Optional) Save the imported DataFrame as CSV in /data
-    # save_path = os.path.join(data_path, "dataset_from_gaia.csv")
-    # df.to_csv(save_path, index=False)
-    # print(f"Dataset saved to {save_path}")
 
     # OPTION 2: Import from local CSV in data folder
     #data_path = data_path()
     #dataset = os.path.join(data_path, "dataset.csv")
     #df = pd.read_csv(dataset)
 
+    # Data cleaning
+    df_cleaned = nans_elimination(df)
+
+    # Save the imported DataFrame as CSV in /data
+    save_dataframe(df_cleaned, data_folder, filename="dataset_from_gaia.csv")
+
     # Preview
     print("\nImported dataset preview:")
+    print(df_cleaned.head())
+
+#STAGE 2: Data Classification
+def stage2():
+    """
+    Main script for classification of the dataset.
+    """
+    # Get data path
+    data_folder = data_path()
+    dataset = os.path.join(data_folder, "dataset_from_gaia.csv")
+    df = pd.read_csv(dataset)
+
+    # Add color and magnitude indices
+    df = add_color_magnitude_indices(df)
+
+    # Label stars
+    df["target"] = df.apply(label_star, axis=1)
+
+    # Star counts
+    star_counts(df)
+
+    # Save the imported DataFrame as CSV in /data
+    save_dataframe(df, data_folder, filename="classified_dataset.csv")
+
+    # Preview
+    print("\nClassified dataset preview:")
     print(df.head())
 
 
@@ -54,3 +85,5 @@ if __name__ == '__main__':
     
     if 1 in stages:
         stage1()
+    elif 2 in stages:
+        stage2()
